d640584bfcfd8a8e4c870b0dbfeb4c8c7afcc77d
João Frois <joao.victor.frois@gmail.com>
2022-09-03 14:11:38 -0300

    Teste com alguns repos

--- LAB02/01-Codigo/middle.sh
+++ LAB02/01-Codigo/middle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Stop on any error
-set -e
+# set -e
 
 #Informações do Repositorio
 NAME_REPO="$1" 
@@ -12,6 +12,7 @@ RELEASES_REPO="$5"
 
 
 #Leitura das colunas corretas do arquivo class.csv
+echo -e "\033[1;35m Gerando arquivos para cada metrica  do Repositorio\033[0m"
 awk -F "\"*,\"*" '{print $4}' ./data/ck_metrics/class.csv > ./data/ck_metrics/cbo.csv
 awk -F "\"*,\"*" '{print $9}' ./data/ck_metrics/class.csv > ./data/ck_metrics/dit.csv
 awk -F "\"*,\"*" '{print $12}' ./data/ck_metrics/class.csv > ./data/ck_metrics/lcom.csv
@@ -24,6 +25,7 @@ LCOM_TOTAL=0
 LOC_TOTAL=0
 
 #Calculo de CBO
+echo -e "\033[1;34m Calculando CBO\033[0m"
 COUNT_LINE=0
 while read CBO; do
     if [ $COUNT_LINE -ne 0 ]; then
@@ -33,6 +35,7 @@ while read CBO; do
 done < "./data/ck_metrics/cbo.csv"
 
 #Calculo de DIT
+echo -e "\033[1;34m Calculando DIT\033[0m"
 COUNT_LINE=0
 while read DIT; do
     if [ $COUNT_LINE -ne 0 ]; then
@@ -42,6 +45,7 @@ while read DIT; do
 done < "./data/ck_metrics/dit.csv"
 
 #Calculo de LCOM
+echo -e "\033[1;34m Calculando LCOM\033[0m"
 COUNT_LINE=0
 while read LCOM; do
     # if [ $COUNT_LINE -ne 0 ]; then
@@ -51,6 +55,7 @@ while read LCOM; do
 done < "./data/ck_metrics/lcom.csv"
 
 #Calculo de LOC
+echo -e "\033[1;34m Calculando LOC\033[0m"
 COUNT_LINE=0
 while read LOC; do
     if [ $COUNT_LINE -ne 0 ]; then
@@ -59,9 +64,11 @@ while read LOC; do
     ((COUNT_LINE=COUNT_LINE+1))
 done < "./data/ck_metrics/loc.csv"
 
+echo -e "\033[1;34m Calculando Quanitdade de Classes\033[0m"
 CLASS_QUANT=0
 ((CLASS_QUANT=COUNT_LINE-1))
 
 #Call end.sh
 #Escrita do .csv final
+echo -e "\033[1;32m Escrevendo dados no .csv\033[0m"
 ./end.sh ${NAME_REPO} ${URL_REPO} ${DATE_REPO} ${STARS_REPO} ${RELEASES_REPO} ${CBO_TOTAL} ${DIT_TOTAL} ${LCOM_TOTAL} ${LOC_TOTAL} ${CLASS_QUANT}
