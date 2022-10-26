ecb236a5f354e1cf113e89d1a5de6f2a9118e2f0
padraorezende <padraonxgalo@gmail.com>
2022-10-26 15:52:14 -0300

    ataualizando graficos

--- LAB03/01-Codigo/graphics-metrics/main.py
+++ LAB03/01-Codigo/graphics-metrics/main.py
@@ -1,58 +1,101 @@
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as stats
 from datetime import datetime as dt
-
-COLUMNS = ['changedFiles', 'body', 'comments.totalCount']
+import numpy as np
 
 df = pd.read_csv('./pullRequest.csv')
 
+def calculator_age(created_at: str) -> float:
+    now = dt.now()
+    created_at = dt.strptime(created_at, '%Y-%m-%dT%H:%M:%SZ')
+    return (now - created_at).days * 24 + (now - created_at).seconds/3600
 
-def calculator_hours(date: str) -> float:
-    date = dt.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
-    return (date).hour
-
-
-# first_time = dt.datetime.now()
-# later_time = dt.datetime.now()
-# difference = later_time - first_time
-# dt.timedelta(0, 8, 562000)
-# seconds_in_day = 24 * 60 * 60
-# divmod(difference.days * seconds_in_day + difference.seconds, 60)
-# (0, 8)
+df['hours_spent']  = df['createdAt'].apply(calculator_age) -  df['closedAt'].apply(calculator_age)
 
 
-# df['hours_spent'] = df['closedAt'].apply(
-#     calculator_hours) - df['createdAt'].apply(calculator_hours)
+
+plt.style.use('_mpl-gallery')
+if not os.path.exists('graphs/'):
+    os.makedirs('graphs/')
+
+
+plt.figure(figsize=(10, 5))
+plt.scatter(df['body'], df['reviews.totalCount'], color="#ab8fdc")
+plt.title("Reviews x Body")
+plt.ylabel("Reviews")
+plt.xlabel("Body")
+plt.savefig(
+    f'graphs/reviews&body.png', dpi=300, bbox_inches='tight')
+
+plt.figure(figsize=(10, 5))
+plt.scatter(df['changedFiles'], df['reviews.totalCount'], color="#ab8fdc")
+plt.title("Reviews x changedFiles")
+plt.ylabel("Reviews")
+plt.xlabel("changedFiles")
+plt.savefig(
+    f'graphs/reviews&changedFiles.png', dpi=300, bbox_inches='tight')
+
+plt.figure(figsize=(10, 5))
+plt.scatter(df['comments.totalCount'], df['reviews.totalCount'], color="#ab8fdc")
+plt.title("Reviews x changedFiles")
+plt.ylabel("Reviews")
+plt.xlabel("changedFiles")
+plt.savefig(
+    f'graphs/reviews&comments.png', dpi=300, bbox_inches='tight')
+
+plt.figure(figsize=(10, 5))
+plt.scatter(df['comments.totalCount'], df['hours_spent'], color="#ab8fdc")
+plt.title("Reviews x hours")
+plt.ylabel("Reviews")
+plt.xlabel("hours")
+plt.savefig(
+    f'graphs/reviews&ghours.png', dpi=300, bbox_inches='tight')
+
+
 
 df_merged = df[df['state'] == 'MERGED']
 df_closed = df[df['state'] == 'CLOSED']
 
-reviews = df['reviews.totalCount']
-for col in COLUMNS:
-    print('Working on:',  col)
-    # A
-    closed = df_closed[col].to_list()
-    merged = df_merged[col].to_list()
 
-    fig, ax = plt.subplots()
-    data = [closed, merged]
-    ax.boxplot(data, labels=['closed', 'merged'], showfliers=False, whis=1.5)
-    ax.set_title(col)
-    plt.savefig('out/A_' + col + '.png')
-    plt.close()
 
-    # B
-    fig, ax = plt.subplots()
-    x = df[col]
-    y = reviews
-    spearman = stats.spearmanr(x, y)
-    title = 'Spearman: ' + str(round(spearman[0], 2))
-    ax.scatter(x, y, alpha=0.5)
-    ax.set(
-        xlabel=col,
-        ylabel='reviews',
-        title=title
-    )
-    plt.savefig('out/B_' + col + '.png')
-    plt.close()
+plt.figure(figsize=(10, 5))
+closed = df_closed['body'].to_list()
+merged = df_merged['body'].to_list()
+data = [closed, merged]
+plt.boxplot(data, labels=['closed', 'merged'], showfliers=False, whis=1.5, patch_artist=True,  boxprops=dict(facecolor="#C7BAFC", color="#8047e2"), medianprops=dict(color="#8047e2"), whiskerprops=dict(color="#8047e2"), capprops=dict(color="#8047e2"))
+plt.savefig(
+    f'graphs/body&status.png', dpi=300, bbox_inches='tight')
+
+
+
+plt.figure(figsize=(10, 5))
+closed = df_closed['changedFiles'].to_list()
+merged = df_merged['changedFiles'].to_list()
+data = [closed, merged]
+plt.boxplot(data, labels=['closed', 'merged'], showfliers=False, whis=1.5, patch_artist=True,  boxprops=dict(facecolor="#C7BAFC", color="#8047e2"), medianprops=dict(color="#8047e2"), whiskerprops=dict(color="#8047e2"), capprops=dict(color="#8047e2"))
+plt.savefig(
+    f'graphs/changedFiles&status.png', dpi=300, bbox_inches='tight')
+
+
+plt.figure(figsize=(10, 5))
+closed = df_closed['comments.totalCount'].to_list()
+merged = df_merged['comments.totalCount'].to_list()
+data = [closed, merged]
+plt.boxplot(data, labels=['closed', 'merged'], showfliers=False, whis=1.5, patch_artist=True,  boxprops=dict(facecolor="#C7BAFC", color="#8047e2"), medianprops=dict(color="#8047e2"), whiskerprops=dict(color="#8047e2"), capprops=dict(color="#8047e2"))
+plt.savefig(
+    f'graphs/comments&status.png', dpi=300, bbox_inches='tight')
+
+
+plt.figure(figsize=(10, 5))
+closed = df_closed['hours_spent'].to_list()
+merged = df_merged['hours_spent'].to_list()
+data = [closed, merged]
+plt.boxplot(data, labels=['closed', 'merged'], showfliers=False, whis=1.5, patch_artist=True,  boxprops=dict(facecolor="#C7BAFC", color="#8047e2"), medianprops=dict(color="#8047e2"), whiskerprops=dict(color="#8047e2"), capprops=dict(color="#8047e2"))
+plt.savefig(
+    f'graphs/hours&status.png', dpi=300, bbox_inches='tight')
+
+
+
+
